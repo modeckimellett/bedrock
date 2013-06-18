db260a5197935eb96b3312a6ffff225dbc4f8309
Mark Daugherty <mark.r.daugherty@gmail.com>
2013-06-18 15:51:20 -0500

    added missing test.

--- cq-library-core/src/test/groovy/com/citytechinc/cq/library/content/resource/predicates/ResourceTypePredicateSpec.groovy
+++ cq-library-core/src/test/groovy/com/citytechinc/cq/library/content/resource/predicates/ResourceTypePredicateSpec.groovy
@@ -6,7 +6,28 @@
 package com.citytechinc.cq.library.content.resource.predicates
 
 import com.citytechinc.cq.library.testing.specs.AbstractCqSpec
+import spock.lang.Unroll
 
 class ResourceTypePredicateSpec extends AbstractCqSpec {
 
+    def setupSpec() {
+        nodeBuilder.content {
+            citytechinc("sling:resourceType": "page")
+        }
+    }
+
+    @Unroll
+    def "resource with matching resource type is included"() {
+        setup:
+        def resource = resourceResolver.getResource("/content/citytechinc")
+        def predicate = new ResourceTypePredicate(resourceType)
+
+        expect:
+        predicate.apply(resource) == result
+
+        where:
+        resourceType | result
+        "page"       | true
+        "node"       | false
+    }
 }
