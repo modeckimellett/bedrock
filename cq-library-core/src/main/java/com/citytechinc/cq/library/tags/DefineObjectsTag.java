5b5137344bc17d92e05a9a16e7cdfe99cfbd7309
Mark Daugherty <mark.r.daugherty@gmail.com>
2013-12-17 15:21:04 -0600

    updated component instantiation.

--- cq-library-core/src/main/java/com/citytechinc/cq/library/tags/DefineObjectsTag.java
+++ cq-library-core/src/main/java/com/citytechinc/cq/library/tags/DefineObjectsTag.java
@@ -14,10 +14,16 @@ import com.day.cq.wcm.api.components.Component;
 import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.scripting.SlingScriptHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.jsp.JspTagException;
+
+import static com.day.cq.wcm.tags.DefineObjectsTag.DEFAULT_CURRENT_PAGE_NAME;
+import static com.day.cq.wcm.tags.DefineObjectsTag.DEFAULT_PAGE_MANAGER_NAME;
+import static com.day.cq.wcm.tags.DefineObjectsTag.DEFAULT_REQUEST_NAME;
+import static com.day.cq.wcm.tags.DefineObjectsTag.DEFAULT_SLING_NAME;
 
 /**
  * Add to and/or override attributes set in pageContext for use in JSPs.
@@ -32,8 +38,6 @@ public final class DefineObjectsTag extends AbstractComponentInstanceTag {
 
     public static final String ATTR_COMPONENT_INSTANCE_NAME = "componentInstanceName";
 
-    public static final String ATTR_CURRENT_PAGE = "currentPage";
-
     public static final String ATTR_IS_AUTHOR = "isAuthor";
 
     public static final String ATTR_IS_EDIT_MODE = "isEditMode";
@@ -43,12 +47,6 @@ public final class DefineObjectsTag extends AbstractComponentInstanceTag {
     public static final String ATTR_IS_PREVIEW_MODE = "isPreviewMode";
 
     public static final String ATTR_IS_PUBLISH = "isPublish";
-
-    public static final String ATTR_PAGE_MANAGER = "pageManager";
-
-    public static final String ATTR_SLING_REQUEST = "slingRequest";
-
-    public static final String ATTR_SLING_RESPONSE = "slingResponse";
 
     public static final String ATTR_IS_DEBUG = "isDebug";
 
@@ -61,7 +59,7 @@ public final class DefineObjectsTag extends AbstractComponentInstanceTag {
     @Override
     public int doEndTag() throws JspTagException {
         final SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) pageContext.getAttribute(
-            ATTR_SLING_REQUEST);
+            DEFAULT_REQUEST_NAME);
 
         final WCMMode mode = WCMMode.fromRequest(slingRequest);
         final boolean isPublish = mode.equals(WCMMode.DISABLED);
@@ -77,15 +75,17 @@ public final class DefineObjectsTag extends AbstractComponentInstanceTag {
 
         if (LOG.isDebugEnabled()) {
             final Resource resource = request.getResource();
+            final SlingScriptHelper sling = (SlingScriptHelper) pageContext.getAttribute(DEFAULT_SLING_NAME);
+            final String scriptResourcePath = sling.getScript().getScriptResource().getPath();
 
-            LOG.debug("doEndTag() instantiated component request for resource = {} with type = {}", resource.getPath(),
-                resource.getResourceType());
+            LOG.debug("doEndTag() instantiated component request for resource path = {} with type = {} and script = {}",
+                new Object[]{ resource.getPath(), resource.getResourceType(), scriptResourcePath });
         }
 
         pageContext.setAttribute(ATTR_COMPONENT_REQUEST, request);
         pageContext.setAttribute(ATTR_COMPONENT_NODE, request.getComponentNode());
-        pageContext.setAttribute(ATTR_PAGE_MANAGER, request.getPageManager());
-        pageContext.setAttribute(ATTR_CURRENT_PAGE, request.getCurrentPage());
+        pageContext.setAttribute(DEFAULT_PAGE_MANAGER_NAME, request.getPageManager());
+        pageContext.setAttribute(DEFAULT_CURRENT_PAGE_NAME, request.getCurrentPage());
 
         instantiateComponentClass(request);
 
@@ -93,52 +93,56 @@ public final class DefineObjectsTag extends AbstractComponentInstanceTag {
     }
 
     private void instantiateComponentClass(final ComponentRequest request) {
-        try {
-            final Component component = request.getComponent();
+        final Component component = request.getComponent();
 
-            if (component != null) {
-                final String className = component.getProperties().get(ComponentConstants.PROPERTY_CLASS_NAME,
-                    String.class);
+        if (component != null) {
+            try {
+                setInstance(component);
 
-                if (className != null) {
-                    final Class<?> clazz = Class.forName(className);
+                Component superComponent = component.getSuperComponent();
 
-                    if (clazz.isAnnotationPresent(AutoInstantiate.class)) {
-                        final String instanceName = getInstanceName(clazz);
-                        final Object instance = getInstance(clazz);
+                while (superComponent != null) {
+                    setInstance(superComponent);
 
-                        pageContext.setAttribute(instanceName, instance);
-                        pageContext.setAttribute(ATTR_COMPONENT_INSTANCE_NAME, instanceName);
-                    } else {
-                        LOG.debug(
-                            "instantiateComponentClass() annotation not present for class name = {}, not instantiating component class",
-                            className);
-                    }
-                } else {
-                    LOG.debug(
-                        "instantiateComponentClass() class name attribute not present for component = {}, not instantiating component class",
-                        component.getResourceType());
+                    superComponent = superComponent.getSuperComponent();
                 }
-            } else {
-                LOG.debug("instantiateComponentClass() component is null, not instantiating component class");
+            } catch (Exception e) {
+                LOG.error("error instantiating component class", e);
             }
-        } catch (Exception e) {
-            LOG.error("error instantiating component class", e);
+        } else {
+            LOG.debug("instantiateComponentClass() component is null, not instantiating component class");
+        }
+    }
+
+    private void setInstance(final Component component) throws Exception {
+        final String className = component.getProperties().get(ComponentConstants.PROPERTY_CLASS_NAME, String.class);
+
+        if (className != null) {
+            final Class<?> clazz = Class.forName(className);
+
+            if (clazz.isAnnotationPresent(AutoInstantiate.class)) {
+                final String instanceName = getInstanceName(clazz);
+                final Object instance = getInstance(clazz);
+
+                LOG.debug("setInstance() class name = {}, instance name = {}, setting component in page context",
+                    className, instanceName);
+
+                pageContext.setAttribute(instanceName, instance);
+                pageContext.setAttribute(ATTR_COMPONENT_INSTANCE_NAME, instanceName);
+            } else {
+                LOG.debug("setInstance() annotation not present for class name = {}, not instantiating component class",
+                    className);
+            }
+        } else {
+            LOG.debug("setInstance() no class name property for component = {}, not instantiating component class",
+                component.getResourceType());
         }
     }
 
     private String getInstanceName(final Class<?> clazz) {
         final AutoInstantiate autoInstantiate = clazz.getAnnotation(AutoInstantiate.class);
-        final String instanceName;
+        final String instanceName = autoInstantiate.instanceName();
 
-        if (autoInstantiate.instanceName().isEmpty()) {
-            instanceName = StringUtils.uncapitalize(clazz.getSimpleName());
-        } else {
-            instanceName = autoInstantiate.instanceName();
-        }
-
-        LOG.debug("getInstanceName() component instance name = {}", instanceName);
-
-        return instanceName;
+        return instanceName.isEmpty() ? StringUtils.uncapitalize(clazz.getSimpleName()) : instanceName;
     }
 }
