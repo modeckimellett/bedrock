5ea97c813a349b2696388d2093b5a11a1ee494b5
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-06-23 15:50:00 -0500

    updated bindings handler.

--- bedrock-core/src/main/groovy/com/citytechinc/aem/bedrock/core/bindings/ComponentBindingValuesProvider.groovy
+++ bedrock-core/src/main/groovy/com/citytechinc/aem/bedrock/core/bindings/ComponentBindingValuesProvider.groovy
@@ -1,8 +1,10 @@
 package com.citytechinc.aem.bedrock.core.bindings
 
 import org.apache.felix.scr.annotations.Component
+import org.apache.felix.scr.annotations.Reference
 import org.apache.felix.scr.annotations.Service
 import org.apache.sling.scripting.api.BindingsValuesProvider
+import org.osgi.framework.Constants
 
 import javax.script.Bindings
 
@@ -10,8 +12,14 @@ import javax.script.Bindings
 @Service(BindingsValuesProvider)
 final class ComponentBindingValuesProvider implements BindingsValuesProvider {
 
+    @Reference(target = "(${Constants.SERVICE_PID}=com.day.cq.wcm.core.impl.WCMBindingsValuesProvider)")
+    protected BindingsValuesProvider wcmBindingValuesProvider
+
     @Override
     void addBindings(final Bindings bindings) {
+        // ensure WCM bindings are added
+        wcmBindingValuesProvider.addBindings(bindings)
+
         bindings.putAll(new ComponentBindings(bindings))
     }
 }
