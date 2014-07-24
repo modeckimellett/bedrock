bad4939a22d8f3be040ff6832290531f62883a21
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-07-24 10:07:38 -0500

    added service provider interface.

---
+++ bedrock-core/src/main/groovy/com/citytechinc/aem/bedrock/core/services/impl/DefaultServiceProvider.groovy
@@ -0,0 +1,30 @@
+package com.citytechinc.aem.bedrock.core.services.impl
+
+import com.citytechinc.aem.bedrock.core.services.ServiceProvider
+import com.google.common.collect.ImmutableList
+import org.apache.sling.api.scripting.SlingScriptHelper
+import org.apache.sling.scripting.core.ScriptHelper
+import org.osgi.framework.BundleContext
+
+final class DefaultServiceProvider implements ServiceProvider {
+
+    private final SlingScriptHelper sling
+
+    DefaultServiceProvider(SlingScriptHelper sling) {
+        this.sling = sling
+    }
+
+    DefaultServiceProvider(BundleContext bundleContext) {
+        sling = new ScriptHelper(bundleContext, null)
+    }
+
+    @Override
+    public <T> T getService(Class<T> serviceType) {
+        sling.getService(serviceType)
+    }
+
+    @Override
+    public <T> List<T> getServices(Class<T> serviceType, String filter) {
+        ImmutableList.of(sling.getServices(serviceType, filter))
+    }
+}
---
+++ bedrock-core/src/main/java/com/citytechinc/aem/bedrock/core/services/ServiceProvider.java
@@ -0,0 +1,28 @@
+package com.citytechinc.aem.bedrock.core.services;
+
+import java.util.List;
+
+/**
+ * OSGi service provider.
+ */
+public interface ServiceProvider {
+
+    /**
+     * Get an OSGi service.
+     *
+     * @param serviceType the type (class) of the service
+     * @param <T> type
+     * @return the service instance, or null if it is not available
+     */
+    <T> T getService(Class<T> serviceType);
+
+    /**
+     * Get one or more OSGi services.
+     *
+     * @param serviceType the type (class) of the service
+     * @param filter optional filter, see OSGi spec for syntax details
+     * @param <T> type
+     * @return one or more service instances, or null if none are found
+     */
+    <T> List<T> getServices(Class<T> serviceType, String filter);
+}
