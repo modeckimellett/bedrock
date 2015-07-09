0d3a9187b5f6702d0bd98fe95c6a20c0902781fe
Mark Daugherty <mark.r.daugherty@gmail.com>
2015-07-09 14:15:14 -0500

    refactored replication service.

--- bedrock-core/src/main/groovy/com/citytechinc/aem/bedrock/core/servlets/replication/SelectiveReplicationServlet.groovy
+++ bedrock-core/src/main/groovy/com/citytechinc/aem/bedrock/core/servlets/replication/SelectiveReplicationServlet.groovy
@@ -1,13 +1,11 @@
 package com.citytechinc.aem.bedrock.core.servlets.replication
 
+import com.citytechinc.aem.bedrock.core.services.SelectiveReplicationService
 import com.citytechinc.aem.bedrock.core.servlets.AbstractJsonResponseServlet
-import com.day.cq.replication.AgentIdFilter
 import com.day.cq.replication.AgentManager
 import com.day.cq.replication.ReplicationActionType
 import com.day.cq.replication.ReplicationException
-import com.day.cq.replication.ReplicationOptions
 import com.day.cq.replication.Replicator
-import groovy.util.logging.Slf4j
 import org.apache.felix.scr.annotations.Reference
 import org.apache.felix.scr.annotations.sling.SlingServlet
 import org.apache.sling.api.SlingHttpServletRequest
@@ -19,7 +17,6 @@ import javax.servlet.ServletException
 import static com.google.common.base.Preconditions.checkArgument
 
 @SlingServlet(paths = "/bin/replicate/selective")
-@Slf4j("LOG")
 class SelectiveReplicationServlet extends AbstractJsonResponseServlet {
 
     private static final String PARAMETER_ACTION = "action"
@@ -34,51 +31,31 @@ class SelectiveReplicationServlet extends AbstractJsonResponseServlet {
     @Reference
     Replicator replicator
 
+    @Reference
+    SelectiveReplicationService replicationService
+
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws
         ServletException, IOException {
-        def paths = request.getParameterValues(PARAMETER_PATHS)
-        def agentIds = request.getParameterValues(PARAMETER_AGENT_IDS)
-
-        checkArgument(paths as Boolean, "paths parameter must be non-null and non-empty")
-        checkArgument(agentIds as Boolean, "agentIds parameter must be non-null and non-empty")
-
+        def paths = request.getParameterValues(PARAMETER_PATHS) as LinkedHashSet
+        def agentIds = request.getParameterValues(PARAMETER_AGENT_IDS) as LinkedHashSet
         def action = request.getParameter(PARAMETER_ACTION)
-        def actionType = ReplicationActionType.fromName(action)
 
-        checkArgument(actionType as Boolean, "invalid action parameter = %s", action)
-
-        def options = getReplicationOptions(agentIds)
+        checkArgument(paths as Boolean, "paths parameter must be non-null and non-empty" as Object)
+        checkArgument(agentIds as Boolean, "agentIds parameter must be non-null and non-empty" as Object)
+        checkArgument(action as Boolean, "invalid action parameter = %s", action)
 
         def session = request.resourceResolver.adaptTo(Session)
+        def actionType = ReplicationActionType.fromName(action)
 
-        def uniquePaths = paths as LinkedHashSet
-
-        uniquePaths.each { path ->
-            LOG.info "executing replication action = $actionType for path = $path to agent IDs = $agentIds"
-
+        paths.each { path ->
             try {
-                replicator.replicate(session, actionType, path, options)
+                replicationService.replicate(session, actionType, path, agentIds)
             } catch (ReplicationException e) {
-                LOG.error "error executing replication action = $actionType for path = $path", e
-
                 throw new ServletException(e)
             }
         }
 
-        writeJsonResponse(response, uniquePaths)
-    }
-
-    private def getReplicationOptions(agentIds) {
-        def uniqueAgentIds = agentIds as LinkedHashSet
-
-        checkArgument(agentManager.agents.keySet().containsAll(uniqueAgentIds),
-            "invalid agent IDs, one or more of the provided agent IDs does not exist")
-
-        def replicationOptions = new ReplicationOptions()
-
-        replicationOptions.filter = new AgentIdFilter(uniqueAgentIds.toArray() as String[])
-
-        replicationOptions
+        writeJsonResponse(response, paths)
     }
 }
