62e04a4649119764ca825a73894bb4efd3cb4cdb
Michael Hodgdon <mhodgdon@citytechinc.com>
2015-07-21 14:07:07 -0400

    fixing error on parameterized types

--- bedrock-models/src/main/groovy/com/citytechinc/aem/bedrock/models/impl/InheritInjector.groovy
+++ bedrock-models/src/main/groovy/com/citytechinc/aem/bedrock/models/impl/InheritInjector.groovy
@@ -2,6 +2,7 @@ package com.citytechinc.aem.bedrock.models.impl
 
 import com.citytechinc.aem.bedrock.api.node.ComponentNode
 import com.citytechinc.aem.bedrock.models.annotations.InheritInject
+
 import org.apache.felix.scr.annotations.Component
 import org.apache.felix.scr.annotations.Property
 import org.apache.felix.scr.annotations.Service
@@ -12,6 +13,7 @@ import org.apache.sling.models.spi.injectorspecific.InjectAnnotationProcessorFac
 import org.osgi.framework.Constants
 
 import java.lang.reflect.AnnotatedElement
+import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
 @Component
@@ -19,42 +21,42 @@ import java.lang.reflect.Type
 @Property(name = Constants.SERVICE_RANKING, intValue = 4000)
 class InheritInjector extends AbstractComponentNodeInjector implements InjectAnnotationProcessorFactory {
 
-    @Override
-    String getName() {
-        InheritInject.NAME
-    }
+	@Override
+	String getName() {
+		InheritInject.NAME
+	}
 
-    @Override
-    Object getValue(ComponentNode componentNode, String name, Type declaredType, AnnotatedElement element,
-        DisposalCallbackRegistry callbackRegistry) {
+	@Override
+	Object getValue(ComponentNode componentNode, String name, Type declaredType, AnnotatedElement element,
+			DisposalCallbackRegistry callbackRegistry) {
 
-        if (declaredType instanceof Class && declaredType.enum) {
-            def enumString = componentNode.getInherited(name, String)
+		if (declaredType instanceof Class && declaredType.enum) {
+			def enumString = componentNode.getInherited(name, String)
 
-            return enumString.present ? declaredType[enumString.get()] : null
-        } else {
-            return componentNode.getInherited(name, declaredType).orNull()
-        }
-    }
+			return enumString.present ? declaredType[enumString.get()] : null
+		} else if(!declaredType instanceof ParameterizedType) {
+			return componentNode.getInherited(name, declaredType).orNull()
+		}
+	}
 
-    @Override
-    InjectAnnotationProcessor createAnnotationProcessor(Object adaptable, AnnotatedElement element) {
-        def annotation = element.getAnnotation(InheritInject)
+	@Override
+	InjectAnnotationProcessor createAnnotationProcessor(Object adaptable, AnnotatedElement element) {
+		def annotation = element.getAnnotation(InheritInject)
 
-        annotation ? new InheritAnnotationProcessor(annotation) : null
-    }
+		annotation ? new InheritAnnotationProcessor(annotation) : null
+	}
 
-    private static class InheritAnnotationProcessor extends AbstractInjectAnnotationProcessor {
+	private static class InheritAnnotationProcessor extends AbstractInjectAnnotationProcessor {
 
-        private final InheritInject annotation
+		private final InheritInject annotation
 
-        InheritAnnotationProcessor(InheritInject annotation) {
-            this.annotation = annotation
-        }
+		InheritAnnotationProcessor(InheritInject annotation) {
+			this.annotation = annotation
+		}
 
-        @Override
-        Boolean isOptional() {
-            annotation.optional()
-        }
-    }
+		@Override
+		Boolean isOptional() {
+			annotation.optional()
+		}
+	}
 }
